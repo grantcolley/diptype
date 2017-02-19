c3324314bb1271339b1e83a69f1129cca71cee9c
grantcolley <grantcolley@yahoo.com>
2017-02-19 21:03:04 +0000

    Initial upload.
    Initial upload.

---
+++ DevelopmentInProgress.DipType/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DipType")]
+[assembly: AssemblyDescription("A type helper that gets and sets properties faster than reflection.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Development In Progress Ltd")]
+[assembly: AssemblyProduct("DipType")]
+[assembly: AssemblyCopyright("Copyright © Development In Progress Ltd 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5f4fba0-2dd1-4a18-8f7c-6ac02697c009")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DevelopmentInProgress.DipType/TypeHelper.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace DevelopmentInProgress.DipType
+{
+    public abstract class TypeHelperBase
+    {
+        public abstract object GetValue(object obj, string propertyName);
+        public abstract void SetValue(object obj, string propertyName, object value);
+    }
+
+    public static class TypeHelper
+    {
+        private static AssemblyBuilder assembly;
+
+        private static ModuleBuilder module;
+
+        private static int counter;
+
+        public static TypeHelperBase Create<T>()
+        {
+            if (assembly == null)
+            {
+                AssemblyName name = new AssemblyName("TypeHelper");
+                assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Run);
+                module = assembly.DefineDynamicModule(name.Name);
+            }
+
+            TypeAttributes attribs = typeof (TypeHelperBase).Attributes;
+
+            TypeBuilder tb = module.DefineType("TypeHelper." + typeof (T).Name + "." + GetNextCounterValue(),
+                (attribs | TypeAttributes.Sealed | TypeAttributes.Public) &
+                ~(TypeAttributes.Abstract | TypeAttributes.NotPublic), typeof (TypeHelperBase));
+
+            var ctor = tb.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, Type.EmptyTypes);
+            var ctorIL = ctor.GetILGenerator();
+            ctorIL.Emit(OpCodes.Newobj, ctor);
+            ctorIL.Emit(OpCodes.Ret);
+
+            MethodInfo baseGetValue = typeof (TypeHelperBase).GetMethod("GetValue");
+
+            MethodBuilder getValueBody = tb.DefineMethod(baseGetValue.Name,
+                baseGetValue.Attributes & ~MethodAttributes.Abstract,
+                typeof (object), new Type[] {typeof (object), typeof (string)});
+
+            var getValueIL = getValueBody.GetILGenerator();
+
+            IEnumerable<PropertyInfo> propertyInfos = GetPropertyInfos<T>();
+
+            GetSetValueIL<T>(getValueIL, propertyInfos, true);
+
+            tb.DefineMethodOverride(getValueBody, baseGetValue);
+
+            MethodInfo baseSetValue = typeof (TypeHelperBase).GetMethod("SetValue");
+
+            MethodBuilder setValueBody = tb.DefineMethod(baseSetValue.Name,
+                baseSetValue.Attributes & ~MethodAttributes.Abstract,
+                typeof (object), new Type[] {typeof (object), typeof (string)});
+
+            var setValueIL = setValueBody.GetILGenerator();
+
+            GetSetValueIL<T>(setValueIL, propertyInfos, false);
+
+            tb.DefineMethodOverride(setValueBody, baseGetValue);
+
+            var typeHelper = (TypeHelperBase) Activator.CreateInstance(tb.CreateType(), Type.EmptyTypes);
+
+            return typeHelper;
+        }
+
+        private static int GetNextCounterValue()
+        {
+            return Interlocked.Increment(ref counter);
+        }
+
+        private static void GetSetValueIL<T>(ILGenerator il, IEnumerable<PropertyInfo> propertyInfos, bool isGet)
+        {
+            var numberOfProperties = propertyInfos.Count();
+
+            var labels = new Label[numberOfProperties];
+
+            for (int i = 0; i < numberOfProperties; i++)
+            {
+                labels[0] = il.DefineLabel();
+            }
+
+            for (int i = 0; i < numberOfProperties; i++)
+            {
+                il.Emit(OpCodes.Ldarg_1);
+                il.Emit(OpCodes.Ldstr, propertyInfos.ElementAt(i).Name);
+                il.Emit(OpCodes.Beq, labels[i]);
+            }
+
+            il.Emit(OpCodes.Ldstr, typeof (T).Name + " doesn't have a property called ");
+            il.Emit(OpCodes.Newobj, typeof (ArgumentOutOfRangeException).GetConstructor(new Type[] {typeof (string)}));
+            il.Emit(OpCodes.Throw);
+
+            for (int i = 0; i < numberOfProperties; i++)
+            {
+                var property = propertyInfos.ElementAt(i);
+
+                il.MarkLabel(labels[i]);
+
+                il.Emit(OpCodes.Ldarg_0);
+
+                if (isGet)
+                {
+                    var getAccessor = property.GetGetMethod();
+
+                    il.EmitCall(OpCodes.Callvirt, getAccessor, null);
+
+                    if (property.PropertyType.IsValueType)
+                    {
+                        il.Emit(OpCodes.Box, property.PropertyType);
+                    }
+                }
+                else
+                {
+                    var setAccessor = property.GetGetMethod();
+
+                    il.Emit(OpCodes.Ldarg_1);
+
+                    if (property.PropertyType.IsValueType)
+                    {
+                        il.Emit(OpCodes.Unbox, property.PropertyType);
+                    }
+
+                    il.EmitCall(OpCodes.Callvirt, setAccessor, null);
+                }
+
+                il.Emit(OpCodes.Ret);
+            }
+        }
+
+        private static IEnumerable<PropertyInfo> GetPropertyInfos<T>()
+        {
+            var propertyInfoResults = new List<PropertyInfo>();
+
+            PropertyInfo[] propertyInfos = typeof (T).GetProperties();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                if (UnsupportedProperty(propertyInfo))
+                {
+                    continue;
+                }
+
+                propertyInfoResults.Add(propertyInfo);
+            }
+
+            return propertyInfoResults;
+        }
+
+        private static bool UnsupportedProperty(PropertyInfo propertyInfo)
+        {
+            // Skip non-public properties and properties that are either 
+            // classes (but not strings), interfaces, lists, generic 
+            // lists or arrays.
+            var propertyType = propertyInfo.PropertyType;
+
+            if (propertyType != typeof (string)
+                && (propertyType.IsClass
+                    || propertyType.IsInterface
+                    || propertyType.IsArray
+                    || propertyType.GetInterfaces()
+                        .Any(
+                            i =>
+                                (i.GetTypeInfo().Name.Equals(typeof (IEnumerable).Name)
+                                 || (i.IsGenericType &&
+                                     i.GetGenericTypeDefinition().Name.Equals(typeof (IEnumerable<>).Name))))))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
